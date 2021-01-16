88977ed791561a8fc5b9dffc5a847688415d3e1c
Dee Adebayo <deeadebayo@gmail.com>
2021-01-16 05:22:15 -0500

    added most of footer content to component

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,15 +1,112 @@
 import React from 'react';
-import styled from 'styled-components';
+import { Row, Col, Container } from 'react-bootstrap';
+import { css } from '@emotion/react';
+import { Link } from 'gatsby';
 
-const StyledFooter = styled.div``;
-
-const Footer = () => (
-	<StyledFooter>
-		<p>Stuff is here</p>
-		<p>Christ the King Medical Center</p>
-		<p>Number and other info</p>
-		<p>The bottom part of the site</p>
-	</StyledFooter>
-);
+const footer = css`
+		display: grid;
+		.footer__links {
+		}
+		.footer__company {
+			border-top: '1px solid #DDDDDD';
+		}
+	`,
+	footerLinksData = [
+		{
+			id: 1,
+			headerTitle: 'About',
+			links: [
+				{ name: 'Doctor', url: '/about/doctor' },
+				{ name: 'Values', url: '/about/values' },
+			],
+		},
+		{
+			id: 2,
+			headerTitle: 'Resources',
+			links: [
+				{ id: 1, name: 'Billing', url: '/patient-resources/billing' },
+				{
+					id: 2,
+					name: 'First Visit',
+					url: '/patient-resources/first-visit',
+				},
+				{
+					id: 3,
+					name: 'Forms',
+					url: '/patient-resources/forms',
+				},
+			],
+		},
+		{
+			id: 3,
+			headerTitle: 'Resources',
+			links: [
+				{ id: 1, name: 'Billing', url: '/patient-resources/billing' },
+				{
+					id: 2,
+					name: 'First Visit',
+					url: '/patient-resources/first-visit',
+				},
+				{
+					id: 3,
+					name: 'Forms',
+					url: '/patient-resources/forms',
+				},
+			],
+		},
+	],
+	Footer = () => (
+		<footer css={footer}>
+			<Container>
+				<Row className='footer__links'>
+					{footerLinksData.map((linkData) => (
+						<Col>
+							<section>
+								<h2>{linkData.headerTitle}</h2>
+								<ul>
+									{linkData.links.map((link) => (
+										<li key={link.id}>
+											<Link to={link.url}>
+												{link.name}
+											</Link>
+										</li>
+									))}
+								</ul>
+							</section>
+						</Col>
+					))}
+				</Row>
+				<hr />
+				<Row className='footer__company'>
+					<Col>
+						<p>
+							© {2021} Christ the King Healthcare. All rights
+							reserved · Privacy · Credits · Sitemap
+						</p>
+					</Col>
+					<Col>
+						<p>Call us icon and google business icon</p>
+					</Col>
+				</Row>
+			</Container>
+		</footer>
+	);
 
 export default Footer;
+
+// 	.page-footer {
+// 		height: 215px;
+// 	/*    height: auto;*/
+// 		background: #56636a;
+// 		border-top: 6px solid #FF724A;
+// 	}
+
+// 	@media (max-width: 767px) {
+// 		.page-footer {
+// 			height: 320px;
+// 		}
+// 	}
+
+// 	.page-footer p {
+// 		color: #fff;
+// 	}
