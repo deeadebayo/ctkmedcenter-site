fb6d8cab5be15494ca94687f391fdb609794b467
Dee Adebayo <deeadebayo@gmail.com>
2021-08-16 01:03:18 -0400

    status: minor changes to banner before pack

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,33 +1,42 @@
 import React from 'react';
-import { Container, Navbar } from 'react-bootstrap';
+import { Col, Container, Row } from 'react-bootstrap';
 import { css } from '@emotion/react';
 import { getFormattedHealthyDate } from '../utils/formattedDate';
 
-
 const bannerStyle = css`
-grid-row: 1/2;
 	padding-bottom: 100px;
-	nav {
-		display: grid;
-		grid-template-columns: 3fr 1fr 1fr;
-	}`
 
-export default function Banner({date}) {
+	.banner__contact {
+		display: flex;
+		flex-flow: row;
+		gap: 1.5em;
+		justify-content: space-around;
+	}
+`;
+
+export default function Banner({ date }) {
 	return (
 		<header css={bannerStyle}>
-			<Navbar variant='light' fixed='top'>
-				<Container>
-					<p>Today is {getFormattedHealthyDate(date)}. It's a great day to be healthy!</p>
-					<p>
-						📞<a href='tel:7705548015'>770 554 8015</a>
-					</p>
-					<p>
-						<a href='mailto:info@ctkmedcenter.com?subject=General%20Inquiry'>
-							info@ctkmedcenter.com
-						</a>
-					</p>
-				</Container>
-			</Navbar>
+			<Container>
+				<Row className='justify-content-between'>
+					<Col>
+						<p>
+							Today is {getFormattedHealthyDate(date)}. It's a
+							great day to be healthy!
+						</p>
+					</Col>
+					<Col sm={4} className='banner__contact'>
+						<span>
+							📞<a href='tel:7705548015'>770 554 8015</a>
+						</span>
+						<span>
+							<a href='mailto:info@ctkmedcenter.com?subject=General%20Inquiry'>
+								info@ctkmedcenter.com
+							</a>
+						</span>
+					</Col>
+				</Row>
+			</Container>
 		</header>
 	);
 }
