5f3e33ac81ae7aaba2031f4496538533ca172e32
Dee Adebayo <deeadebayo@gmail.com>
2021-08-16 01:35:32 -0400

    status: initial setup. Added map display

--- src/components/ContactMap.js
+++ src/components/ContactMap.js
@@ -1,12 +1,23 @@
 import React from 'react';
+import GoogleMap from 'google-map-react';
 import { Col, Row } from 'react-bootstrap';
+import { css } from '@emotion/react';
+
+const googleMapOptions = {
+	defaultCenter: { lat: 33.82128, lng: -83.876151 },
+	defaultZoom: 15,
+	// googleMapsApiKey,
+	// https://github.com/google-map-react/google-map-react
+};
+
+const contactMapStyles = css`
+	height: 400px;
+`;
 
 const ContactMap = () => (
-	<Row>
-		<Col>Health Records</Col>
-		<Col>Get Directions</Col>
-		<Col>Request Appointment</Col>
-		<Col>Billing and Insurance</Col>
+	<Row css={contactMapStyles}>
+		<p>This is the map below</p>
+		<GoogleMap {...googleMapOptions}></GoogleMap>
 	</Row>
 );
 
